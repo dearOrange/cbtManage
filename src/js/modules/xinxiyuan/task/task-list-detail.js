03260b8009e603278fbaa84b2f23a51010633d7f
jiaguishan <jiaguishan@chebutou.com.cn>
2017-12-19 19:11:35 +0800

    任务管理详情页面判断修改

--- src/js/modules/xinxiyuan/task/task-list-detail.js
+++ src/js/modules/xinxiyuan/task/task-list-detail.js
@@ -27,105 +27,127 @@ define(function(require, exports, module) {
                     var html = jh.utils.template('task_detail_template', returnData);
                     $('.taskListContent').html(html);
                     _this.initSheriff();
-					
-				
+
+
                 }
             });
         };
-		this.initSheriff = function() {
+        this.initSheriff = function() {
             jh.utils.ajax.send({
                 url: '/operator/getAllChannel',
                 done: function(returnData) {
                     var strTemplate = jh.utils.template('xx_task_list', returnData);
                     $('.task-content').html(strTemplate);
-                    $('body').off('click','.checkId').on('click','.checkId',function(){
-                    	var checks = jh.utils.getURLValue().args;
+                    $('body').off('click', '.checkId').on('click', '.checkId', function() {
+                        var checks = jh.utils.getURLValue().args;
                     })
-                    
+
                 }
             });
         };
-        
+
         this.registerEvent = function() {
             //信息修复
-            $('body').off('click','.auditClue').on('click','.auditClue',function(){
-            	jh.utils.alert({
-                	content:'确定审核吗？',
-                	ok:function(){
-                		jh.utils.ajax.send({
-			                url: '/trace/adopt',
-			                done: function(returnData) {
-			                    jh.utils.alert({
-									content: '已审核！',
-									ok: function(){
-                                    	window.location.reload();
+            $('body').off('click', '.auditClue').on('click', '.auditClue', function() {
+                jh.utils.alert({
+                    content: '确定审核吗？',
+                    ok: function() {
+                        jh.utils.ajax.send({
+                            url: '/trace/adopt',
+                            done: function(returnData) {
+                                jh.utils.alert({
+                                    content: '已审核！',
+                                    ok: function() {
+                                        window.location.reload();
                                     }
-								})
-			                }
-                
-            			});
-                	},
-                	cancel:true
+                                })
+                            }
+
+                        });
+                    },
+                    cancel: true
                 })
             });
-            
+
             //情报全部拒绝
-            $('body').off('click','.rejectClue').on('click','.rejectClue',function(){
-            	jh.utils.alert({
-                	content:'确定全部不通过吗？',
-                	ok:function(){
-                		jh.utils.ajax.send({
-			                url: '/task/refuseAll',
-			                data: {
-			                	taskId: args.id
-			                },
-			                done: function(returnData) {
-			                    jh.utils.alert({
-									content: '已全部拒绝！',
-									ok: function(){
-                                    	window.location.reload();
+            $('body').off('click', '.rejectClue').on('click', '.rejectClue', function() {
+                jh.utils.alert({
+                    content: '确定全部不通过吗？',
+                    ok: function() {
+                        jh.utils.ajax.send({
+                            url: '/task/refuseAll',
+                            data: {
+                                taskId: args.id
+                            },
+                            done: function(returnData) {
+                                jh.utils.alert({
+                                    content: '已全部拒绝！',
+                                    ok: function() {
+                                        window.location.reload();
                                     }
-								})
-			                }
-                
-            			});
-                	},
-                	cancel:true
+                                })
+                            }
+
+                        });
+                    },
+                    cancel: true
                 })
             });
-            
-          
+
+
             //采纳情报
-            $('body').off('click','.adopteInfo').on('click','.adopteInfo',function(){
-            	jh.utils.alert({
-                	content:'确定采纳吗？',
-                	ok:function(){
-                		var managerId = $(".managerId").filter(":checked");
-                		var checkId = $(".checkId").filter(":checked").val();
-                		var adoptData = {
-                			taskId: args.id,
-                			traceId:checkId,
-                			carPrice:$("#salvage").val(),
-                			estimatedMinPrice: $("#minMoney").val(),
-                			estimatedMaxPrice: $("#maxMoney").val(),
-                			channelManagerId: managerId.val(),
-                			channelManagerName:managerId.data('name')
-                		};
-                		jh.utils.ajax.send({
-			                url: '/task/adopt',
-			                data: adoptData,
-			                done: function(returnData) {
-			                    jh.utils.alert({
-									content: '已采纳！',
-									ok: function(){
-                                    	window.location.reload();
+            $('body').off('click', '.adopteInfo').on('click', '.adopteInfo', function() {
+                var managerId = $(".managerId").filter(":checked");
+                var checkId = $.trim($(".checkId").filter(":checked").val());
+                var carPrice = $.trim($("#salvage").val());
+                var estimatedMinPrice = $.trim($("#minMoney").val());
+                var estimatedMaxPrice = $.trim($("#maxMoney").val());
+                if (!checkId) {
+                    jh.utils.confirm({
+                        content: '请选择具体线索！'
+                    });
+                    return false;
+                }
+                if (!managerId || !$.trim(managerId.val())) {
+                    jh.utils.confirm({
+                        content: '请选择相应渠道经理！'
+                    });
+                    return false;
+                }
+                if (!carPrice || !estimatedMinPrice || !estimatedMaxPrice) {
+                    jh.utils.confirm({
+                        content: '请预估费用！'
+                    });
+                    return false;
+                }
+
+                jh.utils.alert({
+                    content: '确定采纳吗？',
+                    ok: function() {
+                        var adoptData = {
+                            taskId: args.id,
+                            traceId: checkId,
+                            carPrice: carPrice,
+                            estimatedMinPrice: estimatedMinPrice,
+                            estimatedMaxPrice: estimatedMaxPrice,
+                            channelManagerId: managerId.val(),
+                            channelManagerName: managerId.data('name')
+                        };
+                        jh.utils.ajax.send({
+                            url: '/task/adopt',
+                            data: adoptData,
+                            done: function(returnData) {
+                                jh.utils.alert({
+                                    content: '已采纳！',
+                                    ok: function() {
+                                        window.location.reload();
                                     }
-								})
-			                }
-                
-            			});
-                	},
-                	cancel:true
+                                })
+                            }
+
+                        });
+                    },
+                    cancel: true
                 })
             });
         };
