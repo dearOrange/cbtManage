6bad49f7ce3d23c321f409c20a7dc50a23e67f21
jiaguishan <jiaguishan@chebutou.com.cn>
2018-01-02 16:58:13 +0800

    手气任务添加条件

--- src/js/modules/xinxiyuan/luck/luck-list.js
+++ src/js/modules/xinxiyuan/luck/luck-list.js
@@ -8,24 +8,41 @@
 define(function(require, exports, module) {
     function LuckList() {
         var _this = this;
+        _this.form = $('#luck-list-form');
 
         this.init = function() {
-        	this.initContent();
+            this.initContent();
+            this.registerEvent();
         };
 
-		this.initContent = function() {
+        this.initContent = function() {
             var page = new jh.ui.page({
                 data_container: $('#luck_list_container'),
                 page_container: $('#page_container'),
                 method: 'post',
                 url: '/trace/luckTask',
                 contentType: 'application/json',
-                data: {},
+                data: jh.utils.formToJson($('#luck-list-form')),
                 callback: function(data) {
                     return jh.utils.template('luckList_content_template', data);
                 }
             });
             page.init();
+        };
+
+        this.registerEvent = function() {
+            $('select').select2({
+                minimumResultsForSearch: Infinity
+            });
+
+            // 搜索
+            jh.utils.validator.init({
+                id: 'luck-list-form',
+                submitHandler: function(form) {
+                    _this.initContent();
+                    return false;
+                }
+            });
         };
     }
     module.exports = LuckList;
