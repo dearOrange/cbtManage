a29c1e44ea20ed6b140c173cc954529bd686c3be
jiaguishan <jiaguishan@chebutou.com.cn>
2018-02-08 11:13:45 +0800

    分页优化

--- src/js/modules/xinxiyuan/clue/clue-manage.js
+++ src/js/modules/xinxiyuan/clue/clue-manage.js
@@ -108,7 +108,7 @@ define(function(require, exports, module) {
             });
 
             //切换状态
-            $('body').off('click', '.taskState').on('click', '.taskState', function(event,parame) {
+            $('body').off('click', '.taskState').on('click', '.taskState', function(event,param) {
                 $(this).addClass("active").siblings().removeClass("active");
                 $('select').select2();
                 $('#state').val($(this).data('value'));
