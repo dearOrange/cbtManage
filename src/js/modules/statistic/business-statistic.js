dbb957cf6998bb79822cfb6b4bf5db9817ee63b8
pengjun <pengjun1216@163.com>
2018-03-29 17:25:14 +0800

    统计页面—+页面修改

--- src/js/modules/statistic/business-statistic.js
+++ src/js/modules/statistic/business-statistic.js
@@ -8,12 +8,12 @@
 define(function(require, exports, module) {
     function BusinessStatistic() {
         var _this = this;
-//      var date = new Date();
-//      var now = {
-//          year: date.getFullYear(),
-//          month: date.getMonth() + 1
-//      };
-//      now.month = now.month.toString().length === 1 ? '0' + now.month : now.month; //月份两位数
+        var date = new Date();
+        var now = {
+            year: date.getFullYear(),
+            month: date.getMonth() + 1
+        };
+        now.month = now.month.toString().length === 1 ? '0' + now.month : now.month; //月份两位数
 
         _this.traceMonths = [];
         _this.traceCount = [];
@@ -28,13 +28,13 @@ define(function(require, exports, module) {
         _this.countRate = 0;
 
         this.init = function() {
-//          $('#infoTimeInput,#carRecoveryInput,#entrustTimeInput').val(now.year + '-' + now.month);
+            $('#infoTimeInput,#carRecoveryInput,#entrustTimeInput').val(now.year + '-' + now.month);
             this.initHead();
             this.sectionTable();
             this.initSection();
-//          window.initContent('2018-01', true);
-//          window.initClear('2018-01', true);
-//          window.initEntrustSort('2018-01', true);
+            window.initContent('2018-01', true);
+            window.initClear('2018-01', true);
+            window.initEntrustSort('2018-01', true);
             this.registerEvent();
         };
 
@@ -84,68 +84,68 @@ define(function(require, exports, module) {
 
         };
 
-//      window.initContent = function(obj, isSearch) {
-//          obj = typeof obj !== 'object' ? { y: now.year, M: now.month } : obj; //是否为第一次查询
-//          obj.M = obj.M.toString().length === 1 ? '0' + obj.M : obj.M; //月份两位数
-//
-//          var page = new jh.ui.page({
-//              data_container: $('#statistic_container'),
-//              page_container: $('#page_container'),
-//              method: 'post',
-//              url: '/statistics/traceSort',
-//              showPageTotal: false,
-//              jump: false,
-//              show_page_number: 3,
-//              contentType: 'application/json',
-//              data: {
-//                  type: 'trace',
-//                  pageSize: 5,
-//                  yearMonth: obj.y + '-' + obj.M
-//              },
-//              isSearch: isSearch,
-//              callback: function(data) {
-//                  return jh.utils.template('statistic_content_template', data);
-//              }
-//          });
-//          page.init();
-//      };
-//
-//      window.initClear = function(obj, isSearch) {
-//          obj = typeof obj !== 'object' ? { y: now.year, M: now.month } : obj; //是否为第一次查询
-//          obj.M = obj.M.toString().length === 1 ? '0' + obj.M : obj.M; //月份两位数
-//
-//          var page = new jh.ui.page({
-//              data_container: $('#clear_info_container'),
-//              page_container: $('#page_clear_container'),
-//              method: 'post',
-//              url: '/statistics/recoverySort',
-//              contentType: 'application/json',
-//              data: {
-//                pageSize: 5,
-//                  type: 'carRecovery',
-//                  yearMonth: obj.y + '-' + obj.M
-//              },
-//              isSearch: isSearch,
-//              show_page_number: 3,
-//              callback: function(data) {
-//                  return jh.utils.template('clear_content_template', data);
-//              }
-//          });
-//          page.init();
-//      };
+        window.initContent = function(obj, isSearch) {
+            obj = typeof obj !== 'object' ? { y: now.year, M: now.month } : obj; //是否为第一次查询
+            obj.M = obj.M.toString().length === 1 ? '0' + obj.M : obj.M; //月份两位数
 
-//      window.initEntrustSort = function(obj, isSearch) {
-//          obj = typeof obj !== 'object' ? { y: now.year, M: now.month } : obj; //是否为第一次查询
-//          obj.M = obj.M.toString().length === 1 ? '0' + obj.M : obj.M; //月份两位数
-//          jh.utils.ajax.send({
-//              method: 'post',
-//              url: '/statistics/entrust',
-//              contentType: 'application/json',
-//              data: {
-//                  yearMonth: obj.y + '-' + obj.M
-//              },
-//              isSearch: isSearch,
-//              done: function(returnData) {
+            var page = new jh.ui.page({
+                data_container: $('#statistic_container'),
+                page_container: $('#page_container'),
+                method: 'post',
+                url: '/statistics/traceSort',
+                showPageTotal: false,
+                jump: false,
+                show_page_number: 3,
+                contentType: 'application/json',
+                data: {
+                    type: 'trace',
+                    pageSize: 5,
+                    yearMonth: obj.y + '-' + obj.M
+                },
+                isSearch: isSearch,
+                callback: function(data) {
+                    return jh.utils.template('statistic_content_template', data);
+                }
+            });
+            page.init();
+        };
+
+        window.initClear = function(obj, isSearch) {
+            obj = typeof obj !== 'object' ? { y: now.year, M: now.month } : obj; //是否为第一次查询
+            obj.M = obj.M.toString().length === 1 ? '0' + obj.M : obj.M; //月份两位数
+
+            var page = new jh.ui.page({
+                data_container: $('#clear_info_container'),
+                page_container: $('#page_clear_container'),
+                method: 'post',
+                url: '/statistics/recoverySort',
+                contentType: 'application/json',
+                data: {
+                  pageSize: 5,
+                    type: 'carRecovery',
+                    yearMonth: obj.y + '-' + obj.M
+                },
+                isSearch: isSearch,
+                show_page_number: 3,
+                callback: function(data) {
+                    return jh.utils.template('clear_content_template', data);
+                }
+            });
+            page.init();
+        };
+
+        window.initEntrustSort = function(obj, isSearch) {
+            obj = typeof obj !== 'object' ? { y: now.year, M: now.month } : obj; //是否为第一次查询
+            obj.M = obj.M.toString().length === 1 ? '0' + obj.M : obj.M; //月份两位数
+            jh.utils.ajax.send({
+                method: 'post',
+                url: '/statistics/entrust',
+                contentType: 'application/json',
+                data: {
+                    yearMonth: obj.y + '-' + obj.M
+                },
+                isSearch: isSearch,
+                done: function(returnData) {
 //                  var entrust = returnData.data;
 //                  var noResultBox = $('#entrustNoResultBox');
 //                  if (!entrust.length) {
@@ -158,11 +158,11 @@ define(function(require, exports, module) {
 //                  }
 //                  var entrustContent = jh.utils.template('entrust_content_template', returnData);
 //                  $('#entrustResultBox').html(entrustContent);
-//                  
-//
-//              }
-//          });
-//      };
+                    
+
+                }
+            });
+        };
 
         this.registerEvent = function() {
 
@@ -174,14 +174,14 @@ define(function(require, exports, module) {
     /**
      * 情报begin
      */
-//  window.statisticTraceMonthing = function() {
-//      var obj = $dp.cal.newdate;
-//      window.initContent(obj, true);
-//  };
-//  window.statisticTraceYearing = function() {
-//      var obj = $dp.cal.newdate;
-//      window.initContent(obj, true);
-//  };
+    window.statisticTraceMonthing = function() {
+        var obj = $dp.cal.newdate;
+        window.initContent(obj, true);
+    };
+    window.statisticTraceYearing = function() {
+        var obj = $dp.cal.newdate;
+        window.initContent(obj, true);
+    };
     /**
      * 情报end
      */
@@ -189,14 +189,14 @@ define(function(require, exports, module) {
     /**
      * 渠道委托begin
      */
-//  window.statisticEntrustMonthing = function() {
-//      var obj = $dp.cal.newdate;
-//      window.initEntrustSort(obj, true);
-//  };
-//  window.statisticEntrustYearing = function() {
-//      var obj = $dp.cal.newdate;
-//      window.initEntrustSort(obj, true);
-//  };
+    window.statisticEntrustMonthing = function() {
+        var obj = $dp.cal.newdate;
+        window.initEntrustSort(obj, true);
+    };
+    window.statisticEntrustYearing = function() {
+        var obj = $dp.cal.newdate;
+        window.initEntrustSort(obj, true);
+    };
     /**
      * 渠道委托end
      */
@@ -204,14 +204,14 @@ define(function(require, exports, module) {
     /**
      * 车辆清收begin
      */
-//  window.statisticRecoveryMonthing = function() {
-//      var obj = $dp.cal.newdate;
-//      window.initClear(obj, true);
-//  };
-//  window.statisticRecoveryYearing = function() {
-//      var obj = $dp.cal.newdate;
-//      window.initClear(obj, true);
-//  };
+    window.statisticRecoveryMonthing = function() {
+        var obj = $dp.cal.newdate;
+        window.initClear(obj, true);
+    };
+    window.statisticRecoveryYearing = function() {
+        var obj = $dp.cal.newdate;
+        window.initClear(obj, true);
+    };
     /**
      * 车辆清收end
      */
--- src/js/modules/statistic/informer-statistic.js
+++ src/js/modules/statistic/informer-statistic.js
@@ -105,46 +105,39 @@ define(function(require, exports, module) {
             var mainCarNum = echarts.init(document.getElementById('mainCarNum'));
             var mainBarNum = echarts.init(document.getElementById('mainBarNum'));
             mainInformate.setOption({
+                color: ['#3398DB'],
                 tooltip : {
-                    trigger: 'axis'
+                    trigger: 'axis',
+                    axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                        type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                    }
                 },
-                legend: {
-                    data:['合规线索','不合规线索']
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
                 },
-                calculable: true,
-                xAxis: [
+                xAxis : [
                     {
                         type : 'category',
-                        data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月','1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+                        data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                        axisTick: {
+                            alignWithLabel: true
+                        }
                     }
                 ],
-                yAxis: [
+                yAxis : [
                     {
                         type : 'value'
                     }
                 ],
-                series: [
+                series : [
                     {
-                        name:'合规线索',
+                        name:'直接访问',
                         type:'bar',
-                        data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3,2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
-                        markPoint : {
-                            data : [
-                                {type : 'max', name: '最大值'},
-                                {type : 'min', name: '最小值'}
-                            ]
-                        }
-                    },
-                    {
-                        name:'不合规线索',
-                        type:'bar',
-                        data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3,2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
-                        markPoint : {
-                            data : [
-                                {name : '年最高', value : 182.2, xAxis: 7, yAxis: 183, symbolSize:18},
-                                {name : '年最低', value : 2.3, xAxis: 11, yAxis: 3}
-                            ]
-                        }
+                        barWidth: '60%',
+                        data:[10, 52, 200, 334, 390, 330, 220]
                     }
                 ]
             });
