6723821879c7212c40b7841a92648dd280e3c9bc
mafuliang <764197467@qq.com>
2018-05-24 15:17:19 +0800

    详情完善

--- src/js/modules/taskFlow/task-flow-detail.js
+++ src/js/modules/taskFlow/task-flow-detail.js
@@ -12,17 +12,15 @@ define(function(require, exports, module) {
 
         this.init = function() {
             this.initContent();
+            this.addRemark();
         };
-        this.initContent = function() {
+        this.initContent = function(){
             jh.utils.ajax.send({
                 url: '/thirdTask/taskDetail',
                 data: {
                     taskId: args.id
                 },
                 done: function(returnData) {
-                    returnData.viewImgRoot = jh.config.viewImgRoot;
-                    returnData.officerState = jh.utils.officerState;
-                    returnData.getPositionByImage = jh.utils.getPositionByImage;
                     var informalStr = jh.utils.template('task_flow_detail_template', returnData);
                     $('.flowDetailContent').html(informalStr);
                     
@@ -31,6 +29,42 @@ define(function(require, exports, module) {
             });
             
         };
+        this.addRemark=function(){
+            // 添加备注
+         $('body').off('click','.addCon').on('click','.addCon',function(){
+            var index=$(this).index();
+            console.log(index);
+             var alertContent=jh.utils.template('addRemark', {});
+             var list=jh.utils.template('addList', {});
+                   jh.utils.alert({
+                      title: '添加备注',
+                      okValue:'保存',
+                     content:alertContent,
+                     ok:function(){
+                          $(list).appendTo($('.arrowConBottom'));
+                     },
+                     cancel: true
+
+                   }) 
+         })
+        };
+        // 点击确认
+       $('body').off('click','.sureCom').on('click','.sureCom',function(){
+             jh.utils.alert({
+                     content:"<span style='margin:10x 0;display:block'>是否确认次任务流程已完成？</span>",
+                     ok:function(){
+                        // console.log(this);
+                           // this.style.color="#000";
+                           console.log(1);
+
+
+                     },
+                     cancel: true
+
+                   }) 
+
+       })
+
     }
     module.exports = TaskFlowDetail;
 });
