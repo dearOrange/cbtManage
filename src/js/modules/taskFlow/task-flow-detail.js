810dc93df1c7b379a230983d28dc061b70dc49d6
ccc123456 <pengjun1216@163.com>
2018-05-24 18:42:24 +0800

    树流程详情—+添加备注

--- src/js/modules/taskFlow/task-flow-detail.js
+++ src/js/modules/taskFlow/task-flow-detail.js
@@ -9,9 +9,10 @@ define(function(require, exports, module) {
     function TaskFlowDetail() {
         var _this = this;
         var args = jh.utils.getURLValue().args;
-
         this.init = function() {
             this.initContent();
+            this.taskProgramOne();
+//          this.taskProgramTwo();
             this.addRemark();
         };
         this.initContent = function(){
@@ -26,53 +27,62 @@ define(function(require, exports, module) {
                     $('.flowDetailContent').html(informalStr);
                 }
             });
-//          jh.utils.ajax.send({
-//              url: '/tree/flowDetail',
-//              data: {
-//                treeId: args.id
-//              },
-//              done: function(returnData) {
-//                  var informalStr = jh.utils.template('task_flow_detail_template', returnData);
-//                  $('.flowDetailContent').html(informalStr);
-//              }
-//          });
-            
         };
+        this.taskProgramOne = function(){
+          jh.utils.ajax.send({
+            url: '/tree/flowDetail',
+            data: {
+              treeId: args.id,
+              state:'issue'
+            },
+            done: function(returnData) {
+              returnData.switchTime = jh.utils.switchTime;
+              var informalStr = jh.utils.template('task_programOne_template', returnData);
+              $('#taskProgram').html(informalStr);
+            }
+          });
+        }
+//      this.taskProgramTwo = function(){
+//        jh.utils.ajax.send({
+//          url: '/tree/flowDetail',
+//          data: {
+//            treeId: args.id,
+//            state:'cluesifte'
+//          },
+//          done: function(returnData) {
+//            returnData.switchTime = jh.utils.switchTime;
+//            var informalStr = jh.utils.template('task_programOne_template', returnData);
+//            $('#taskProgram').html(informalStr);
+//          }
+//        });
+//      };
         this.addRemark=function(){
             // 添加备注
-         $('body').off('click','.addCon').on('click','.addCon',function(){
-            var index=$(this).index();
-            console.log(index);
-             var alertContent=jh.utils.template('addRemark', {});
-             var list=jh.utils.template('addList', {});
-                   jh.utils.alert({
-                      title: '添加备注',
-                      okValue:'保存',
-                     content:alertContent,
-                     ok:function(){
-                        $(list).appendTo($('.arrowConBottom'));
-                     },
-                     cancel: true
-
-                   }) 
-         })
+            $('body').off('click','.addCon').on('click','.addCon',function(){
+              var index=$(this).index();
+              var treeState = $(this).data('state');
+              var alertContent=jh.utils.template('addRemark_template', {});
+              jh.utils.alert({
+                title: '添加备注',
+                okValue:'保存',
+                content:alertContent,
+                ok:function(){
+                  var remarkData = jh.utils.formToJson($('#remark-content-form'));
+                  remarkData.treeId = args.id;
+                  remarkData.state = treeState;
+                  jh.utils.ajax.send({
+                    method:'post',
+                    url: '/tree/remark',
+                    data: remarkData,
+                    done: function(returnData) {
+                      _this.initContent();
+                    }
+                  });
+                },
+                cancel: true
+              }) 
+            })
         };
-        // 点击确认
-       $('body').off('click','.sureCom').on('click','.sureCom',function(){
-             jh.utils.alert({
-                     content:"<span style='margin:10x 0;display:block'>是否确认次任务流程已完成？</span>",
-                     ok:function(){
-                        // console.log(this);
-                           // this.style.color="#000";
-                           console.log(1);
-
-
-                     },
-                     cancel: true
-
-                   }) 
-
-       })
 
     }
     module.exports = TaskFlowDetail;
